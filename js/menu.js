4b138065a9f7d33fe99e601a655b534d5423b58e
nonbinary00 <agarkovaalysh12@gmail.com>
2022-07-17 19:15:30 +0200

    не открывается кнопка

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,29 @@
+// (() => {
+// 	const refs = {
+// 		openMenuBtn: document.querySelector(".menu-open"),
+// 		closeMenuBtn: document.querySelector(".menu-close"),
+// 		menu: document.querySelector(".mob-menu"),
+// 	};
+
+// 	refs.openMenuBtn.addEventListener("click", toggleMenu);
+// 	refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+// 	function toggleMenu() {
+// 		refs.menu.classList.toggle("is-open");
+// 	}
+// })();
+
 (() => {
-	const refs = {
-		openMenuBtn: document.querySelector(".menu-open"),
-		closeMenuBtn: document.querySelector(".menu-close"),
-		menu: document.querySelector(".mob-menu"),
-	};
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+  };
 
-	refs.openMenuBtn.addEventListener("click", toggleMenu);
-	refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.openMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
 
-	function toggleMenu() {
-		refs.menu.classList.toggle("is-open");
-	}
+  function toggleModal() {
+    refs.menu.classList.toggle('is-open');
+  }
 })();
